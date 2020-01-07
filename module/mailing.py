3d4a07bd4fa3cb5a2164c8ffef18c4c6f48c9b98
stralar <larsstratmann@gmx.de>
2020-01-07 11:46:57 +0100

    mailing updated

--- module/mailing.py
+++ module/mailing.py
@@ -3,6 +3,8 @@
 import smtplib
 import json
 import os
+import sys
+
 
 class Mail:
     def __init__(self):
@@ -17,23 +19,36 @@ class Mail:
         self.password = data["password"]
         self.host = data["host"]
         self.port = data["port"]
-        self.receivers = ["receivers"]
+        self.receivers = data["receivers"]
 
 
 
-        # smtplib.SMTP( [host [, port [, local_hostname]]] )
-        self.server = smtplib.SMTP_SSL(self.host, self.port)
-        self.server.ehlo()
-        self.server.login(self.user, self.password)
+
 
     def send_without_file(self):
+
         try:
-            self.server.sendmail(self.user, self.receivers, "Hello 123")
+            # smtplib.SMTP( [host [, port [, local_hostname]]] )
+            self.server = smtplib.SMTP_SSL(self.host, self.port)
+
+            # self.server = smtplib.SMTP(self.host, self.port)
+            # self.server.starttls()
+
+            self.server.ehlo()
+            self.server.login(self.user, self.password)
+
+            self.server.sendmail(self.user, "larsstratmann@gmx.de", "Hello 123")
+            self.server.close()
+            pass
         except:
-            print("Somthing went wron by sending a Email")
+            e = sys.exc_info()[0]
+            print("Unexpected error in email sending: " + e)
+            raise
 
 
 if __name__ == '__main__':
     email = Mail()
+
+    email.send_without_file()
 
 
