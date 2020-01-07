687fdbca2e2e06350d0aab568ea51b63ccae0df5
stralar <larsstratmann@gmx.de>
2020-01-07 15:31:22 +0100

    mailing modul finished

--- module/mailing.py
+++ module/mailing.py
@@ -5,15 +5,17 @@ import json
 import os
 import sys
 
+config_File = "mail_client_real.json"
+#config_File = "mail_client.json"
+
 
 class Mail:
     def __init__(self):
         # load config
-        with open(os.path.dirname(os.path.abspath(__file__)) + '/config/mail_client.json') as json_data_file:
+        with open(os.path.dirname(os.path.abspath(__file__)) + '/config/' + config_File) as json_data_file:
             data = json.load(json_data_file)
 
         print(data)
-
 
         self.user = data["user"]
         self.password = data["password"]
@@ -21,34 +23,27 @@ class Mail:
         self.port = data["port"]
         self.receivers = data["receivers"]
 
-
-
-
-
-    def send_without_file(self):
+    def send_without_file(self, msg):
 
         try:
             # smtplib.SMTP( [host [, port [, local_hostname]]] )
             self.server = smtplib.SMTP_SSL(self.host, self.port)
 
-            # self.server = smtplib.SMTP(self.host, self.port)
-            # self.server.starttls()
-
             self.server.ehlo()
             self.server.login(self.user, self.password)
 
-            self.server.sendmail(self.user, "larsstratmann@gmx.de", "Hello 123")
+            self.server.sendmail(self.user, self.receivers, msg)
             self.server.close()
             pass
         except:
             e = sys.exc_info()[0]
-            print("Unexpected error in email sending: " + e)
+            print("Unexpected error in email sending: " + str(e))
             raise
 
-
 if __name__ == '__main__':
+
     email = Mail()
 
-    email.send_without_file()
+    email.send_without_file("This is a message")
 
 
